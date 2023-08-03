bf5d1a97d024980330a24abb8b146274e1c2a174
razninr <89953784+razninr@users.noreply.github.com>
2023-08-03 20:29:07 +0800

    Add files via upload

--- testagain1.py
+++ testagain1.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1pbg0PINstGGMOkBdfNSkMXDqtfvnIv2j
 """
 
-!pip install streamlit
+#!pip install streamlit
 import streamlit as st
 import pandas as pd
 import pickle
